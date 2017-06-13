99413b6b97b2f6a999723ea6b730cd1248d2af73
JOHNSON CHIANG <chiangys@tw.ibm.com>
2017-06-14 06:48:08 +0800

    Session user module

--- local/apigateway/apis/_common/session.js
+++ local/apigateway/apis/_common/session.js
@@ -1,14 +1,1 @@
-
-
-// function Session() {
-//     let ctx = session.name('apimgr');
-//     this._parameters = ctx ? ctx.getVar("parameters") : null;
-// }
-
-// Object.defineProperty(Session.prototype, 'parameters', {
-//     get: function() {
-//         return this._parameters;
-//     }
-// });
-
 module.exports =  require('local:///gateway/api-gateway/apigw-util.js').Session;
